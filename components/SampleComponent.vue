669d351b144cf26c83b23ada48d5264541a3fbb4
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-15 15:30:38 +0900

    cmini

---
+++ components/SampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+  <p>Current Token: {{ token }}</p>
+  <slot name="area" :token="token" />
+</div>
+</template>
+
+<script>
+const generateToken = () => {
+  return `${Math.ceil(Math.random() * 1e10)}`;
+};
+
+export default {
+  data() {
+    return {
+      token: generateToken()
+    }
+  },
+  beforeMount() {
+    this.token = generateToken();
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <h1>Welcome to our party.</h1>
+  <sample-component>
+    <template v-slot:area="{ token }">
+      <p :data-token="token">Current token (in slot): {{ token }}</p>
+    </template>
+  </sample-component>
+  <button @click="reload">Reload</button>
+</div>
+</template>
+
+<script>
+import SampleComponent from '../components/SampleComponent.vue';
+
+export default {
+  components: {
+    SampleComponent
+  },
+  methods: {
+    reload: () => {
+      location.reload();
+    }
+  }
+}
+</script>
